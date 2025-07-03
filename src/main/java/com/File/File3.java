b96055ca68a215830884007310d783a7f0bde717
karanwaghachoure <karanwaghachoure@gmail.com>
2025-07-03 23:43:27 +0530

    file handle

---
+++ src/main/java/com/File/File3.java
@@ -0,0 +1,13 @@
+package com.File;
+
+import java.io.FileInputStream;
+
+public class File3 {
+	public static void main(String[] args)  throws Exception{
+		FileInputStream fs = new FileInputStream("Ram.txt");
+		System.out.println(fs);
+		fs.close();
+		
+	}
+
+}
